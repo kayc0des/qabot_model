fe2b30f29f246df5050588a3a2c6ae4567ba3de1
kayc0des <k.boafo@alustudent.com>
2024-10-14 13:52:00 +0200

    updated class constructor

--- json_to_df.py
+++ json_to_df.py
@@ -27,23 +27,31 @@ class JSON_to_DF():
         with open(file_path, 'r') as file:
             try:
                 self.data = json.load(file)
-                self.validate_data(self.data)
+
+                if not len(self.data.keys()) == 1:
+                    raise ValueError(f"More than one key found")
+
+                data_key = list(self.data.keys())[0]
+
+                if not data_key == 'intents':
+                    raise ValueError(f"intents key not found")
+                
+                # Check every intent to make sure they each have three keys
+                # ['tag', 'patterns', 'responses']
+                intent_keys = ['tag', 'patterns', 'responses']
+                
+                for intent in self.data['intents']:
+                    if not len(intent.keys()) == 3:
+                        raise ValueError(f"Expected three keys for each intent")
+                    if not list(intent.keys()) == intent_keys:
+                        raise ValueError(f"Each Intent in Intents shouls have three keys (tag, patterns and responses)")
+
+                print('All checks passed')
+
             except json.JSONDecodeError:
                 raise TypeError(f"The file '{file_path}' does not contain valid JSON.")
     
-    def validate_data(self, data):
-        '''
-        Validates self.data to have a key intents and intents is a list of dictionaries
-        and every dict has three keys tag, patterns and responses
-        
-        Args:
-            data: self.data
-        
-        Returns:
-            Calls convert to_df
-        '''
-    
-    def to_df(self, data):
+    def to_df(self):
         '''
         Converts data to a pandas dataframe
         
@@ -74,3 +82,9 @@ class JSON_to_DF():
         Args:
             dir: Directory to save dataframe
         '''
+        
+if __name__ == '__main__':
+    path = 'json/intents.json'
+    jsontodf = JSON_to_DF(path)
+    df = jsontodf.to_df()
+    print(df.head())
