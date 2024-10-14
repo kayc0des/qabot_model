441fed0db0a58409c6b2c4c2cdfe52348d5a5e79
kayc0des <k.boafo@alustudent.com>
2024-10-14 11:03:31 +0200

    validate data

--- json_to_df.py
+++ json_to_df.py
@@ -3,6 +3,7 @@
 
 import json
 import os
+import pandas as pd
 
 
 class JSON_to_DF():
@@ -22,9 +23,46 @@ class JSON_to_DF():
         # Check file extension
         if not file_path.endswith('.json'):
             raise TypeError(f"The file '{file_path}' is not a JSON file.")
-        
+
         with open(file_path, 'r') as file:
             try:
-                self.json = json.load(file)
+                self.data = json.load(file)
+                self.validate_data(self.data)
             except json.JSONDecodeError:
                 raise TypeError(f"The file '{file_path}' does not contain valid JSON.")
+    
+    def validate_data(self, data):
+        '''
+        Validates self.data to have a key intents and intents is a list of dictionaries
+        and every dict has three keys tag, patterns and responses
+        
+        Args:
+            data: self.data
+        
+        Returns:
+            Calls convert to_df
+        '''
+    
+    def to_df(self, data):
+        '''
+        Converts data to a pandas dataframe
+        
+        Args:
+            Data: self.data
+        
+        Return:
+            Df : Pandas Dataframe
+        '''
+        # Convert the intents into a pandas DataFrame
+        data = []
+
+        for intent in self.data['intents']:
+            tag = intent['tag']
+            patterns = ", ".join(intent['patterns'])
+            responses = ", ".join(intent['responses'])
+            data.append([tag, patterns, responses])
+
+        # Create the DataFrame
+        self.df = pd.DataFrame(data, columns=['Tag', 'Patterns', 'Responses'])
+
+        return self.df
