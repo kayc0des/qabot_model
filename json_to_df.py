f0473533088224325b4f6e095c448a71972bfee5
kayc0des <k.boafo@alustudent.com>
2024-10-14 10:48:56 +0200

    check json

--- json_to_df.py
+++ json_to_df.py
@@ -1,10 +1,14 @@
 ''' This Script Contains a class that converts a JSON File into a pandas dataframe '''
 
 
+import json
+import os
+
+
 class JSON_to_DF():
     ''' Converts JSON to DF '''
 
-    def __init__(self, path):
+    def __init__(self, file_path):
         '''
         Constructor method
         
@@ -16,3 +20,11 @@ class JSON_to_DF():
         '''
 
         # Check file extension
+        if not file_path.endswith('.json'):
+            raise TypeError(f"The file '{file_path}' is not a JSON file.")
+        
+        with open(file_path, 'r') as file:
+            try:
+                self.json = json.load(file)
+            except json.JSONDecodeError:
+                raise TypeError(f"The file '{file_path}' does not contain valid JSON.")
