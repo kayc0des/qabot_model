64083ee6e0cda8d40df92c6a7707a3a1b99c4c85
kayc0des <k.boafo@alustudent.com>
2024-10-24 14:49:20 +0200

    qadataset

--- qadataset.py
+++ qadataset.py
@@ -1,20 +1,13 @@
 ''' Create QADataset and DataLoader'''
 
 import torch
-import pandas as pd
-from transformers import BertTokenizer
 from torch.utils.data import Dataset, DataLoader
-from transformers import BertForSequenceClassification, AdamW
-import pickle
-
-tokenize_data = __import__('tokenizer').tokenize_data
-
-data_path = 'data/intents.csv'
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-df = pd.read_csv(data_path)
 
 
 class QADataset(Dataset):
+    '''
+    
+    '''
     def __init__(self, encodings, labels):
         self.encodings = encodings
         self.labels = labels
@@ -26,46 +19,3 @@ class QADataset(Dataset):
         item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
         item['labels'] = torch.tensor(self.labels[idx])
         return item
-
-if __name__ == '__main__':
-    encoded_data, labels, num_labels, tag_to_label = tokenize_data(df, tokenizer=tokenizer)
-    dataset = QADataset(encoded_data, labels)
-    dataloader = DataLoader(dataset, batch_size=8, shuffle=True)
-    model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=num_labels)
-    optimizer = AdamW(model.parameters(), lr=5e-5)
-    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-    model.to(device)
-    model.train()
-
-    epochs = 1
-    for epoch in range(epochs):
-        for batch in dataloader:
-            # Move batch data to the device
-            input_ids = batch['input_ids'].to(device)
-            attention_mask = batch['attention_mask'].to(device)
-            labels = batch['labels'].to(device)
-
-            # Zero the gradients
-            optimizer.zero_grad()
-
-            # Forward pass
-            outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
-            loss = outputs.loss
-            loss.backward()
-
-            optimizer.step()
-
-        print(f"Epoch {epoch+1} completed with loss {loss.item()}")
-    
-    model.eval()
-    
-    # Save the entire model, not just the state dict
-    model_data = {
-        'model': model,
-        'num_labels': num_labels,
-        'tag_to_label': tag_to_label
-    }
-    with open('model/qa_model.pkl', 'wb') as f:
-        pickle.dump(model_data, f)
-    print("Model saved successfully.")
-    
